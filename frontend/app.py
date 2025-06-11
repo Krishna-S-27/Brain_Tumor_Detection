2d2dcde4b248422bb6eb48843f412f2f2ac0f5fd
KRISHNA SHALAWADI <krishnashalawadi2004@outlook.com>
2025-06-11 12:32:14 +0700

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -19,7 +19,7 @@ if uploaded_file:
         try:
             # Prepare image for POST request
             files = {"file": (uploaded_file.name, uploaded_file, uploaded_file.type)}
-            response = requests.post("http://localhost:8000/predict", files=files)
+            response = requests.post("https://brain-tumor-detection-9vzr.onrender.com", files=files)
 
             if response.status_code == 200:
                 result = response.json()
