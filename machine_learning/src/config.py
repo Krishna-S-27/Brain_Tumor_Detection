5306e820df5d6db5af28ccf8ebda116e8d5c37e2
KRISHNA SHALAWADI <krishnashalawadi2004@outlook.com>
2025-06-11 12:59:28 +0700

    Update config.py

--- machine_learning/src/config.py
+++ machine_learning/src/config.py
@@ -13,3 +13,7 @@ IMG_WIDTH = 224
 # Classes (must match folder names in RAW_DIR)
 CLASSES = ['glioma', 'meningioma', 'notumor', 'pituitary']
 CLASS_TO_INDEX = {cls: idx for idx, cls in enumerate(CLASSES)}
+
+CNN_MODEL_URL = "https://www.dropbox.com/scl/fi/hycicdjgwyogidwpkmhin/cnn_model.h5?rlkey=6siy5ttso5cxzqb7xwetk8ve5&st=wvpmpeli&dl=1"
+SVM_MODEL_URL = "https://www.dropbox.com/scl/fi/89ery357pf19i97tjl3c9/svm_model.pkl?rlkey=m64njjqrarqj7s1lp1wld9m9u&st=9n8m0ry1&dl=1"
+RF_MODEL_URL  = "https://www.dropbox.com/scl/fi/rtvw1d9s8v432rip3v4dc/rf_model.pkl?rlkey=bt79zzljcvidel7ns7sxffngr&st=eqfqaye6&dl=1"
